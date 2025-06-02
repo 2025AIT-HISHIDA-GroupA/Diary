6e31a09e48969d4a825a9fe206149b7e6e6c712b
piman528 <k23086kk@aitech.ac.jp>
2025-06-02 16:05:36 +0900

    著者の検索機能の追加

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ from diaries.omagariDiary import omagariDiary
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [DiarySample(),NagataniDiary(),omagariDiary(),YoheiDiary()]
 
+# ユーザーに検索したい著者名を入力させる
+search_author = input("検索したい著者の名前を入力してください: ")
+
+print(f"\n--- {search_author}の日記 ---")
+
 for d in diaries:
-    print("---------------------------------")
-    print(d.get_date())
-    print(d.get_summary())
-    print(d.get_author())
-    print()
+    if d.get_author() == search_author:
+        print("---------------------------------")
+        print(d.get_date())
+        print(d.get_summary())
+        print(d.get_author())
+        print()
