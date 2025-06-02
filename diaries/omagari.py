dcdb690fcf23140723c04c949475f99ca96f13fe
sopralto <k23030kk@aitech.ac.jp>
2025-06-02 14:25:55 +0900

    自分の日記を追加

---
+++ diaries/omagari.py
@@ -0,0 +1,8 @@
+from diaries.AbstractDiary import AbstractDiary
+class NagataniDiary(AbstractDiary):
+    def get_date(self):
+        return "2025-06-02"
+    def get_summary(self):
+        return """グループワークをしました"""
+    def get_author(self):
+        return "omagari"
